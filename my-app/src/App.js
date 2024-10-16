54f20fcede3a9256591a7bffaac39d4ad03d7437
Adithyakiran M Kurup <adithyakiranmkurup@gmail.com>
2024-10-16 21:54:10 +0530

    fourth commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { React, useState } from "react";
+import {nanoid} from "nanoid";
+import {StyleSheet} from "react-native";
 
 function App() {
 
@@ -13,33 +15,40 @@ function App() {
         confirmPassword: '',
         file: null,
     });
-    /*const [image, setImage] = useState({});*/
 
     const [errors, setErrors] = useState({});
+    var [newData, setNewData] = useState([]);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editId, setEditId] = useState(null);
 
-    /*const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [contact, setContact] = useState("");
-    const [gender, setGender] = useState("");
-    const [image, setImage] = useState("");*/
+    const styles = StyleSheet.create({
+        image: {
+            display: "none"
+        }
+    })
 
     const handleChange = (e) => {
          
-        const {name, value, files} = e.target;
+        const {name} = e.target;
         if(name === 'file')
         {
-            setFormData({
-                ...formData,
-                [name]: files[0],
-            });
+            const fieldName = e.target.getAttribute("name");
+            const fieldValue = e.target.files[0];
+
+            const newFormData = {...formData};
+            newFormData[fieldName] = fieldValue;
+
+            setFormData(newFormData);
         }
         else
         {
-            setFormData({
-                ...formData,
-                [name]: value,
-            });
+            const fieldName = e.target.getAttribute("name");
+            const fieldValue = e.target.value;
+
+            const newFormData = {...formData};
+            newFormData[fieldName] = fieldValue;
+
+            setFormData(newFormData);  
         }
     };
 
@@ -47,24 +56,59 @@ function App() {
         e.preventDefault();
         const newErrors = validateForm(formData);
         setErrors(newErrors);
-        /*console.log(
-            formData.firstName,
-            formData.lastName,
-            formData.email,
-            formData.contact,
-            formData.gender,
-            formData.image,
-        );*/
-        setFormData({
-            firstName: '',
-            lastName: '',
-            email: '',
-            contact: '',
-            gender: '',
-            password: '',
-            confirmPassword: '',
-            image: '',
-        });
+        
+        console.log(formData.firstName, formData.lastName, formData.email, formData.contact, formData.gender);
+
+       if(Object.keys(newErrors).length === 0)
+       {
+        if (isEditing) 
+        {
+            setNewData(newData.map((data) => 
+                data.id === editId ? { ...data, ...formData } : data
+            ));
+            setIsEditing(false);
+            setEditId(null);
+        }
+        else
+        {
+            const newDataEntry = {
+                id: nanoid(),
+                firstName: formData.firstName,
+                lastName: formData.lastName,
+                email: formData.email,
+                contact: formData.contact,
+                gender: formData.gender,
+            };
+
+            setNewData([...newData, newDataEntry]);
+
+            document.getElementById("tbl").style.display = "";
+            setFormData({
+                firstName: '',
+                lastName: '',
+                email: '',
+                contact: '',
+                gender: '',
+                password: '',
+                confirmPassword: '',
+                file: '',
+            });
+        }
+            
+       }
+    };
+
+    const handleEdit = (id) => 
+    {
+        const dataToEdit = newData.find((data) => data.id === id);
+        setFormData(dataToEdit);
+        setIsEditing(true);
+        setEditId(id);
+    };
+
+    const handleDelete = (id) => 
+    {
+        setNewData(newData.filter((data) => data.id !== id));
     };
 
     const validateForm = (data) => {
@@ -111,58 +155,87 @@ function App() {
     };
 
     return (
-        <div className="App">
+        <div>
             <h1>FORM IN REACT</h1>
             <fieldset>
-                <form action="#" method="get"  >
+                <form action="#" method="get" className="form">
                     <div>
-                        <label for="firstname">First Name</label>
+                        <label>First Name</label>
                         <input type="text" name="firstName" id="firstname" value={formData.firstName} onChange={handleChange} placeholder="Enter First Name" required/>
                         {errors.firstName && (<span className ="error">{errors.firstName}</span>)}
                     </div>
                     <div>
-                        <label for="lastname">Last Name</label>
+                        <label>Last Name</label>
                         <input type="text" name="lastName" id="lastname" value={formData.lastName} onChange={handleChange} placeholder="Enter Last Name" required/>
                         {errors.lastName && (<span className ="error">{errors.lastName}</span>)}
                     </div>
                     <div>
-                        <label for="email">Enter Email</label>
+                        <label>Enter Email</label>
                         <input type="email" name="email" id="email" value={formData.email} onChange={handleChange} placeholder="Enter email" required/>
                         {errors.email && (<span className ="error">{errors.email}</span>)}
                     </div>
                     <div>
-                        <label for="tel">Contact</label>
+                        <label>Contact</label>
                         <input type="tel" name="contact" id="contact" value={formData.contact} onChange={handleChange} placeholder="Enter Mobile number" required/>
                         {errors.contact && (<span className ="error">{errors.contact}</span>)}
                     </div>
                     <div>
-                        <label for="gender">Gender</label>
-                        <input type="radio" name="gender" value="male" id="male" checked={formData.gender === "male"} onChange={handleChange}/>
+                        <label>Gender</label>
+                        <input type="radio" name="gender" value="Male" id="male" checked={formData.gender === "Male"} onChange={handleChange}/>
                         Male
-                        <input type="radio" name="gender" value="female" id="female" checked={formData.gender === "female"} onChange={handleChange}/>
+                        <input type="radio" name="gender" value="Female" id="female" checked={formData.gender === "Female"} onChange={handleChange}/>
                         Female
-                        <input type="radio" name="gender" value="other" id="other" checked={formData.gender === "other"} onChange={handleChange}/>
+                        <input type="radio" name="gender" value="Other" id="other" checked={formData.gender === "Other"} onChange={handleChange}/>
                         Other<br></br>
                         {errors.gender && (<span className ="error">{errors.gender}</span>)}
                     </div>
                     <div>
-                        <label for="password">Password</label>
+                        <label>Password</label>
                         <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Enter the Password"></input>
                         {errors.password && (<span className="error">{errors.password}</span>)}
                     </div>
                     <div>
-                        <label for="confirmPassword">Confrim Password</label>
+                        <label>Confrim Password</label>
                         <input type="password" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} placeholder="Re-enter the Password"></input>
                         {errors.confirmPassword && (<span className="error">{errors.confirmPassword}</span>)}
                     </div>
                     <div>
-                        <label for="file">Upload Image</label>
+                        <label>Upload Image</label>
                         <input type="file" name="file" id="file" onChange={handleChange} placeholder="Enter Upload File" required/>
                         {errors.image && (<span className ="error">{errors.image}</span>)}
                     </div>
                     <button type="submit" value="Submit" onClick={(e) => handleSubmit(e)}>Submit</button>
                 </form>
             </fieldset>
+
+            <table className="table" style={styles.image} id="tbl">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Contact</th>
+                        <th>Gender</th>
+                    </tr>
+                </thead>
+                <tbody>
+                       {newData.map((data) => (
+                         <tr key={data.id}>
+                            <td>{data.firstName}</td>
+                            <td>{data.lastName}</td>
+                            <td>{data.email}</td>
+                            <td>{data.contact}</td>
+                            <td>{data.gender}</td>
+                            <td>
+                               <div className="button">
+                                    <button onClick={() => handleEdit(data.id)} id="edit">Edit</button>
+                                    <button onClick={() => handleDelete(data.id)} id="delete">Delete</button>
+                               </div>
+                            </td>
+                        </tr>
+                       ))}
+                </tbody>
+            </table>
         </div>
     );
 }
